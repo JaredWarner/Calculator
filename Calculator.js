96a7cac4425d347209753f10871255d5b8d1532d
JaredWarner <jaredwarner279@gmail.com>
2024-09-18 20:54:42 +1000

    Updated Calculator (calc branch): Added Calc function

---
+++ Calculator.js
@@ -0,0 +1,11 @@
+function calculator(num1, num2, op) {
+  const a = num1;
+  const b = num2;
+  const operations = {
+    addition: a + b,
+    subtraction: a - b,
+    multiplication: a * b,
+    division: a / b,
+  };
+  return operations[op];
+}
